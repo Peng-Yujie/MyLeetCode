cf0c1815afbf3bcc46e3020a28b256cb12e70efa
Yujie <124231801+Peng-Yujie@users.noreply.github.com>
2024-04-16 21:16:05 -0700

    Time: 16 ms (10.44%), Space: 20.3 MB (78.11%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -11,20 +11,32 @@
  */
 class Solution {
 public:
+    string res = "{";
     string smallestFromLeaf(TreeNode* root) {
-        if (!root) return "";
-        char ch = (char)('a' + root->val);
-        string str = string(1, ch);
-        return dfs(root, str);
+        dfs(root, "");
+        return res;
     }
-    string dfs(TreeNode* root, string str) {
-        if (!root) return "";
-        string str1, str2;
-        if (root->left) str1 = dfs(root->left, (char)('a' + root->left->val) + str);
-        if (root->right) str2 = dfs(root->right, (char)('a' + root->right->val) + str);
-        if (str1.empty() && str2.empty()) return str;
-        if (str1.empty()) return str2;
-        if (str2.empty()) return str1;
-        return str1 < str2 ? str1 : str2;   
+    void dfs(TreeNode* root, string str) {
+        if (!root) return;
+        str = (char)('a' + root->val) + str;
+        if (!root->left && !root->right) res = res < str ? res : str;
+        dfs(root->left, str);
+        dfs(root->right, str);
     }
+    // string smallestFromLeaf(TreeNode* root) {
+    //     if (!root) return "";
+    //     char ch = (char)('a' + root->val);
+    //     string str = string(1, ch);
+    //     return dfs(root, str);
+    // }
+    // string dfs(TreeNode* root, string str) {
+    //     if (!root) return "";
+    //     string str1, str2;
+    //     if (root->left) str1 = dfs(root->left, (char)('a' + root->left->val) + str);
+    //     if (root->right) str2 = dfs(root->right, (char)('a' + root->right->val) + str);
+    //     if (str1.empty() && str2.empty()) return str;
+    //     if (str1.empty()) return str2;
+    //     if (str2.empty()) return str1;
+    //     return str1 < str2 ? str1 : str2;   
+    // }
 };
