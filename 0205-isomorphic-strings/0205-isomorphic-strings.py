08eb4832b51f233ab40768be1ac9dcd3d35c6f30
Yujie <124231801+Peng-Yujie@users.noreply.github.com>
2024-05-17 13:45:24 -0700

    Time: 38 ms (78.4%), Space: 16.8 MB (24.27%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,12 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        dict1, dict2 = {}, {}
+        dict1= {}
         for i in range(len(s)):
             if not dict1.get(s[i]):
+                if t[i] in dict1.values():
+                    return False
                 dict1[s[i]] = t[i]
-            if not dict2.get(t[i]):
-                dict2[t[i]] = s[i]
-            if dict1[s[i]] != t[i] or dict2[t[i]] != s[i]:
+            if dict1[s[i]] != t[i]:
                 return False
         
         return True
